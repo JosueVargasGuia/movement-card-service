4ceea9b536381d362916d51497210481b8a48dd0
JosueVargasGuia <44180784+JosueVargasGuia@users.noreply.github.com>
2022-05-04 17:24:39 -0500

    Update TypeAccount.java

--- src/main/java/com/nttdata/movementcard/service/entity/TypeAccount.java
+++ src/main/java/com/nttdata/movementcard/service/entity/TypeAccount.java
@@ -2,5 +2,5 @@ package com.nttdata.movementcard.service.entity;
 
 public enum TypeAccount {
 	CreditAccount, BankAccounts,BankTransfers, /** Indica que es una tranferencia interbancaria */externalAccount
-	;
+	;//FIX
 }
